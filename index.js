7f661e786564ed182d2cc4d51d4c29a001d12dc5
Kenneth Latour <kenneth.latourii@gmail.com>
2022-07-16 10:58:37 -0500

    commit tree cd7987cb114f54f1e40fa587bb8e89500f20179c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Ralph")
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift("Bob")
+}
+function appendCat(name){
+    const copyOfCats= [ ...cats, "Broom"]
+    return copyOfCats
+}
+function prependCat(name){
+    const copyOfCats = ["Arnold", ...cats]
+    return copyOfCats
+}
+function removeLastCat(){
+    const copyOfCats = cats.slice(0,2)
+    return copyOfCats
+}
+function removeFirstCat(){
+    const copyOfCat = cats.slice(1)
+    return copyOfCat
+}
